854068f267a81174be5defe73eafacbb587edc07
rodrigo oliveira <pracimaestudos@gmail.com>
2024-11-05 10:28:00 -0300

    stack-1

---
+++ src/conta/Menu.java
@@ -0,0 +1,19 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		
+	Scanner leia = new Scanner(System.in);
+	
+	
+		
+	
+	
+	
+	leia.close();
+	}
+}
